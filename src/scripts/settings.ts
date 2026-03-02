c692a792b9445b68197aeebae1c8fff9118256d0
raphael <raphael.zwick8@gmail.com>
2026-03-02 08:48:03 +0100

    add settings functions --> render user desicion

--- src/scripts/settings.ts
+++ src/scripts/settings.ts
@@ -10,18 +10,33 @@ const htmlIds = {
 
 
 export function testSetting(){
-    let theme1 = document.getElementById('code');
-    let theme2 = document.getElementById('game');
-    theme1?.addEventListener('change', checkInputStatus);
-    theme2?.addEventListener('change', checkInputStatus);
+    //let theme1 = document.getElementById('code');
+    //let theme2 = document.getElementById('game');
+    //let blue = document.getElementById('blue');
+    //let orange = document.getElementById('orange');
+    //let cards = document.getElementsByClassName('checkmark');
+   
+    //theme1?.addEventListener('change', checkInputStatus);
+    //theme2?.addEventListener('change', checkInputStatus);
     
+    const allInputs = document.querySelectorAll('.menu-point input[type="radio"]');
+
+    allInputs.forEach(input =>{
+        input.addEventListener('change',(event)=>{
+            checkInputStatus(event)
+            
+        })
+    })
+
 };
 
 
 function checkInputStatus(event:any){
+    storeUserDecissons();
     const checkbox = event.target;
     if(checkbox.checked ){
         renderThemeImg(checkbox)
+       
     }else{
         
     }
@@ -31,20 +46,42 @@ function renderThemeImg(checkbox:HTMLInputElement){
     let themeImg = document.getElementById('theme-img') as HTMLImageElement;
     if(checkbox.id === 'code'){
         themeImg.src = htmlIds.theme.code
-    }else{
+    }if(checkbox.id ==='game'){
         themeImg.src = htmlIds.theme.gaming
     }
 }
 
-function renderSettingsSummary(){
+
+
+function storeUserDecissons(){
+    let activTheme = document.querySelector('input[name="game"]:checked') as HTMLInputElement;
+    let activPlayer = document.querySelector('input[name="player"]:checked') as HTMLInputElement;
+    let activBoard = document.querySelector('input[name="board"]:checked') as HTMLInputElement;
+    
+    const storeInfos:Object = {
+        'theme': activTheme?.parentElement?.querySelector('p')?.innerText,
+        'player': activPlayer?.parentElement?.querySelector('p')?.innerText,
+        'board': activBoard?.parentElement?.querySelector('p')?.innerText
+    }
+    
+    if(activPlayer && activBoard && activTheme){
+        console.log(storeInfos)
+    }
+    renderDecissionSummary(storeInfos)
+    //return(storeInfos)
+    
 
 }
 
-//function storeUserDecissons(){
-//    let themeImg = document.getElementById('theme-img');
-//    let themeName = document.getElementById('theme-name');
-//    let playerName = document.getElementById('player-color');
-//    let boardSize = document.getElementById('board-size');
-//    
-//}
+function renderDecissionSummary(storeInfos:Object | any){
+    let themeDisplay = document.getElementById('theme-name') as HTMLElement;
+    let player = document.getElementById('player-color') as HTMLElement;
+    let board = document.getElementById('board-size') as HTMLElement;
+
+    //themeDisplay?.innerText = storeInfos.theme
+    themeDisplay.innerText = storeInfos.theme;
+    player.innerText = storeInfos.player;
+    board.innerText = storeInfos.board;
+
+}
 
