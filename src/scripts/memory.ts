b28c11171b62d2d2cac1a8d9bdedd48fb444644b
raphael <raphael.zwick8@gmail.com>
2026-03-08 09:47:57 +0100

    add function for a dynamic card and tablesize

--- src/scripts/memory.ts
+++ src/scripts/memory.ts
@@ -18,6 +18,7 @@ export function getSettingsFromLocaStorage(){
 }
 
 function toggleTableSize(settings:SelectionInfos){
+    let cardtable = document.getElementById('card-table');HTMLElement
     let cardStack = document.getElementById('card-wrapper'); HTMLElement
     let tableSize: string | undefined
     let cardTheme: string | undefined
@@ -26,6 +27,21 @@ function toggleTableSize(settings:SelectionInfos){
         tableSize = settings.board
         cardTheme = settings.theme
         console.log('Kartenanzahl:',tableSize ,'Theme:', cardTheme)
+        if(tableSize?.includes('16')){
+            cardStack?.classList.add('Count-16');
+            cardtable?.classList.add('--size-16')
+        }
+        if(tableSize?.includes('24')){
+            cardStack?.classList.add('Count-24');
+            cardtable?.classList.add('--size-24')
+        }
+        if(tableSize?.includes('36')){
+            cardStack?.classList.add('Count-36');
+            cardtable?.classList.add('--size-36')
+        }
+        else{
+            console.log('fehler in der memory.ts toggleTableSize')
+        }
     }else{
         console.log('Ein Fehler ist in den Settings aufgetretten oder checke den local Storage')
     } 
